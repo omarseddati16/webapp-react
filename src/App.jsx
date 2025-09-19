da0a84807b9ebc4f37b9b408cc06dc62310940ab
omarseddati16 <omarseddati16@gmail.com>
2025-09-19 14:22:51 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import DefaultLayout from './layouts/DefaultLayout'
+import HomePage from './pages/HomePage'
+import DetailFilm from './pages/DetailFilm'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/films/:id" element={<DetailFilm />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter >
+  )
+}
+
+export default App
---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import Main from "../components/Main";
+
+
+function DefaultLayout() {
+  return <>
+    <Header />
+    <Main />
+  </>
+}
+
+export default DefaultLayout
